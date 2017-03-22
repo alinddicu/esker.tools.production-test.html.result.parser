d1361871785b5baf560e38db2b2ff5425b405d26
Alin Dicu <alinddicu@yahoo.com>
2017-03-22 18:14:56 +0100

    more init + htmlagilitypack

--- esker.tools.prod.html.result.parser/esker.tools.prod.html.result.parser/Program.cs
+++ esker.tools.prod.html.result.parser/esker.tools.prod.html.result.parser/Program.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace esker.tools.prod.html.result.parser
+﻿namespace esker.tools.prod.html.result.parser
 {
-	class Program
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Program
 	{
-		static void Main(string[] args)
+		private const string ReferenceFile = @"C:\Users\DicuA\Documents\EOD\Stories\FT-010905\DateFormat\T-1680020001\AttachFiles\Reference\EOD\AttachFile1.htm";
+		private const string CourantFile = @"C:\Users\DicuA\Documents\EOD\Stories\FT-010905\DateFormat\T-1680020001\AttachFiles\Courant\EOD\AttachFile1.htm";
+		private const string DiffsFile = @"C:\Users\DicuA\Documents\EOD\Stories\FT-010905\DateFormat\T-1680020001\AttachFiles\Diffs\EOD\AttachFile1.htm";
+
+		public static void Main(string[] args)
 		{
 		}
 	}
