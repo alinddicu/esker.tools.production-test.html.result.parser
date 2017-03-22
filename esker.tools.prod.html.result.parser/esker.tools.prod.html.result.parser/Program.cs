e10c910647650ea2e4c9d0c8ea1b13d704228468
Alin Dicu <alinddicu@yahoo.com>
2017-03-22 18:10:29 +0100

    init

---
+++ esker.tools.prod.html.result.parser/esker.tools.prod.html.result.parser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esker.tools.prod.html.result.parser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
