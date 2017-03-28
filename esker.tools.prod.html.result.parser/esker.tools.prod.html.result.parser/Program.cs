e74bc2b38f143ecf38f0c6fd63e9af0ddaf42e2a
Alin Dicu <alinddicu@yahoo.com>
2017-03-28 09:11:28 +0200

    no more applicatin exceptio on "Nombre de lignes" différents

--- esker.tools.prod.html.result.parser/esker.tools.prod.html.result.parser/Program.cs
+++ esker.tools.prod.html.result.parser/esker.tools.prod.html.result.parser/Program.cs
@@ -11,22 +11,17 @@
 	public class Program
 	{
 		private const string RootDirectory = @"C:\Users\DicuA\Documents\EOD\Stories\FT-010905\DateFormat\";
-		private const string ReferenceFileTemplate = RootDirectory+ @"{0}\AttachFiles\Reference\EOD\AttachFile1.htm";
+		private const string ReferenceFileTemplate = RootDirectory + @"{0}\AttachFiles\Reference\EOD\AttachFile1.htm";
 		private const string CourantFileTemplate = RootDirectory + @"{0}\AttachFiles\Courant\EOD\AttachFile1.htm";
 		private const string DiffsFileTemplate = RootDirectory + @"{0}\AttachFiles\Diffs\EOD\AttachFile1.htm";
-		
+
 		public static void Main(string[] args)
 		{
-			var testNames = new [] { "T-1680020001", "T-1680020002", "T-1680020003", "T-1680020004", "T-1680020005", "T-1680020006" };
+			var testNames = new[] { "T-1680020001", "T-1680020002", "T-1680020003", "T-1680020004", "T-1680020005", "T-1680020006" };
 			foreach (var testName in testNames)
 			{
 				DoForTest(testName);
 			}
-		}
-
-		private static string GetFileName(string testName, string fileTemplate)
-		{
-			return string.Format(CultureInfo.InvariantCulture, fileTemplate, testName);
 		}
 
 		private static void DoForTest(string testName)
@@ -38,21 +33,18 @@
 			DoForFiles(referenceFile, courantFile, diffsFile);
 		}
 
+		private static string GetFileName(string testName, string fileTemplate)
+		{
+			return string.Format(CultureInfo.InvariantCulture, fileTemplate, testName);
+		}
+
 		private static void DoForFiles(string referenceFile, string courantFile, string diffsFile)
 		{
 			var referenceDataColumns = GetDataColumns(referenceFile).ToArray();
-			var referenceColumnsCount = referenceDataColumns.Length;
-
 			var courantHtmlDoc = GetHtmlDoc(courantFile);
 			var courantDataColumns = GetDataColumns(courantHtmlDoc).ToArray();
-			var courantColumnsCount = courantDataColumns.Length;
 
-			if (referenceColumnsCount != courantColumnsCount)
-			{
-				throw new ApplicationException("Nombre de lignes différents");
-			}
-
-			CheckDataColumns(referenceDataColumns, courantDataColumns);
+			CheckDataLines(referenceDataColumns, courantDataColumns);
 
 			courantHtmlDoc.Save(diffsFile);
 		}
@@ -76,15 +68,14 @@
 			return htmlDocument.DocumentNode.Descendants().Where(d => d.Name == "tr" && !d.InnerHtml.Contains("th"));
 		}
 
-		private static void CheckDataColumns(
-			IReadOnlyList<HtmlNode> referenceDataColumns,
-			IReadOnlyList<HtmlNode> courantDataColumns)
+		private static void CheckDataLines(
+			IReadOnlyList<HtmlNode> referenceDataLines,
+			IReadOnlyList<HtmlNode> courantDataLines)
 		{
-			for (var i = 0; i < referenceDataColumns.Count; i++)
+			for (var i = 0; i < referenceDataLines.Count; i++)
 			{
-				var referenceCells = GetDataCells(referenceDataColumns[i]).ToArray();
-				var courantCells = GetDataCells(courantDataColumns[i]).ToArray();
-
+				var referenceCells = GetDataCells(referenceDataLines[i]).ToArray();
+				var courantCells = GetDataCells(courantDataLines[i]).ToArray();
 				if (referenceCells.Length != courantCells.Length)
 				{
 					throw new ApplicationException("Nombre de cellules différentes sur la ligne " + i);
@@ -99,12 +90,14 @@
 			return dataLine.Descendants().Where(d => d.Name == "td");
 		}
 
-		private static void CheckDataCells(IReadOnlyList<HtmlNode> referenceCells, IReadOnlyList<HtmlNode> courantCells)
+		private static void CheckDataCells(
+			IReadOnlyList<HtmlNode> referenceLineCells,
+			IReadOnlyList<HtmlNode> courantLineCells)
 		{
-			for (var j = 0; j < referenceCells.Count; j++)
+			for (var j = 0; j < referenceLineCells.Count; j++)
 			{
-				var referenceCell = referenceCells[j];
-				var courantCell = courantCells[j];
+				var referenceCell = referenceLineCells[j];
+				var courantCell = courantLineCells[j];
 				if (referenceCell.InnerHtml != courantCell.InnerHtml)
 				{
 					courantCell.SetAttributeValue("style", "background-color:orange");
